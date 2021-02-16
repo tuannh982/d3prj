2168ac9ae8305c18c12414e1427532068d44d813
tuannh982 <tuannh982@gmail.com>
2021-02-16 19:18:57 +0700

    rf

---
+++ src/main/java/com/tuannh/offer/management/domain/entity/user/Staff.java
@@ -0,0 +1,10 @@
+package com.tuannh.offer.management.domain.entity.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Staff {
+    private final String staffId;
+}
